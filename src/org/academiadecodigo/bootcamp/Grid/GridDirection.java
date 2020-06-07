0e1d7c374574f961f3aabd3dbd2dbb56ef63a3bf
Rafael Moreira <13rjmms@gmail.com>
2020-06-07 14:03:39 +0100

    Defining gridDirections

--- src/org/academiadecodigo/bootcamp/Grid/GridDirection.java
+++ src/org/academiadecodigo/bootcamp/Grid/GridDirection.java
@@ -1,13 +1,57 @@
 package org.academiadecodigo.bootcamp.Grid;
 
+import javax.print.DocFlavor;
+
 public enum GridDirection {
     LEFT,
     RIGHT,
-    DOWN;
+    DOWN,
+    UP,
+    ENE,
+    NE,
+    NNE,
+    NNW,
+    NW,
+    WNW,
+    WSW,
+    SW,
+    SSW,
+    SSE,
+    SE,
+    ESE;
 
-    public static GridDirection getnewDirection(GridDirection current) {
 
-        return LEFT;
+    public static GridDirection getNewDirection(GridDirection current) {
+        switch (current){
+            case UP:
+                return DOWN;
+            case NNW:
+                return SSW;
+            case NW:
+                return SW;
+            case WNW:
+                return WSW;
+            case WSW:
+                return ESE;
+            case SW:
+                return SE;
+            case SSW:
+                return SSE;
+            case SSE:
+                return NNE;
+            case SE:
+                return NE;
+            case ESE:
+                return ENE;
+            case ENE:
+                return WNW;
+            case NE:
+                return NW;
+            case NNE:
+                return NNW;
+
+        }
+        return null;
     }
 
 }
