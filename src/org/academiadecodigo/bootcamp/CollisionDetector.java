39d3494eeb000599d003b13e77c414d9add70ec9
Goncalo Pinho <goncalopinho@hotmail.com>
2020-06-08 15:44:03 +0100

    Ball direction

--- src/org/academiadecodigo/bootcamp/CollisionDetector.java
+++ src/org/academiadecodigo/bootcamp/CollisionDetector.java
@@ -82,17 +82,3 @@ public class CollisionDetector {
     }
 
 }
-
-   /*
-            if (ball.getPosition().getPosY() <= brick.getPosition().getPosY() + brick.getPosition().getHeight() &&         //limite de colisão da parte de baixo do tijolo
-                    ball.getPosition().getPosY() + ball.getPosition().getHeight() >= brick.getPosition().getHeight() &&        //limite da colisão da parte de cima do tijolo
-                    ball.getPosition().getPosX() + ball.getPosition().getWidth() >= brick.getPosition().getPosX() &&         //limite de colisão da parte esquerda do tijolo
-                    ball.getPosition().getPosX() <= brick.getPosition().getPosX() + brick.getPosition().getWidth())             //limite de colisão da parte direita do tijolo
-            { */
-// if (ball.getPosition().getPosY().equals(brick.getPosition().getPosY())) {
-
-        /* lógica para de baixo (Se quisermos separar)
-        if (ball.getPosition().getPosY() <= (brick.getPosition().getPosY() + brick.getPosition().getHeight()) &&
-        ball.getPosition().getPosX() >= brick.getPosition().getPosX() &&
-        ball.getPosition().getPosX() + ball.getPosition().getWidth() <= brick.getPosition().getPosX() + brick.getPosition().getWidth()) {
-        */
