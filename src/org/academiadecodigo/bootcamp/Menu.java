cb313ccb32584ecf1627016f8a44fb6498ce1335
Goncalo Pinho <goncalopinho@hotmail.com>
2020-06-10 21:22:08 +0100

    Initial menu

--- src/org/academiadecodigo/bootcamp/Menu.java
+++ src/org/academiadecodigo/bootcamp/Menu.java
@@ -1,19 +1,23 @@
 package org.academiadecodigo.bootcamp;
 
 import org.academiadecodigo.bootcamp.Grid.Grid;
+import org.academiadecodigo.bootcamp.Grid.GridPosition;
 import org.academiadecodigo.bootcamp.Tests.Sound;
 import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 
+import java.util.Timer;
+
 public class Menu implements KeyboardHandler {
 
-    private final String background = "background2.jpg";
+    private final String background = "resources/ACNOID2020.jpg";
 
     private Grid grid;
     private Keyboard keyboard;
     private boolean isRunning = true;
+    private GridPosition textStart;
 
 
     public Menu () {
@@ -30,11 +34,12 @@ public class Menu implements KeyboardHandler {
         inputStart.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
         keyboard.addEventListener(inputStart);
 
+        textStart = grid.makeGridPosition(170, 550, 286, 26, "resources/start.png");
     }
 
     public void start() throws InterruptedException {
         while (isRunning) {
-            Thread.sleep(10);
+            textStart.show();
         }
         Sound.playPipinoDNovo();
         Thread.sleep(1000);
