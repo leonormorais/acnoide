c633584f6b9fe7ffd7426ac864be4053ade1c3a7
Leonor Morais <leonorlmorais@gmail.com>
2020-06-10 23:21:00 +0100

    updated score

--- src/org/academiadecodigo/bootcamp/Score.java
+++ src/org/academiadecodigo/bootcamp/Score.java
@@ -5,12 +5,14 @@ import org.academiadecodigo.bootcamp.Grid.GridPosition;
 
 public class Score {
 
+    private static final int SCORE_POSITION_X = 30;
+    private static final int SCORE_POSITION_Y = 30;
+
     private static int score;
     private static GridPosition position;
 
-
     public static void createGridPosition(Grid grid) {
-        position = grid.makeGridPosition(30, 30, "score");
+        position = grid.makeGridPosition(SCORE_POSITION_X, SCORE_POSITION_Y, "score");
     }
 
     public static void setScore(int sc) {
