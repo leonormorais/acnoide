1c70fb02cd3b2397988ae8a480793ef6d5cd2a7f
Leonor Morais <leonorlmorais@gmail.com>
2020-06-11 12:57:54 +0100

    There's more than Ritas

--- src/org/academiadecodigo/bootcamp/GameObjects/ObjectFactory.java
+++ src/org/academiadecodigo/bootcamp/GameObjects/ObjectFactory.java
@@ -69,7 +69,7 @@ public class ObjectFactory {
             return createSuperBrick(grid);
         }
 
-        BrickType newBrick = bricksType[2];
+        BrickType newBrick = bricksType[random];
         return new SuperBrick(grid.makeGridPosition(posX, posY, BRICK_WIDTH, BRICK_HEIGHT, sourceImg[colorCounter]), newBrick);
 
     }
